52f5029262cb4389bc511becbbdae751fd6f1b47
wright <calebogundiya@gmail.com>
2020-05-14 23:43:30 +0100

    Bulma message toggle

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
 Vue.config.devtools = true;
 
-let app = new Vue({
-  el: "#app",
-  data: {
-    tasks: [
-      { description: "Go to the store", completed: true },
-      { description: "Finish screencast", completed: false },
-      { description: "Make Donation", completed: true },
-      { description: "Clear Inbox", completed: false },
-      { description: "Make dinner", completed: false },
-      { description: "Clean room", completed: true },
-    ],
-  },
-  methods: {
-    toggleLoad() {
-      this.isLoading = !this.isLoading;
-    },
-  },
-  computed: {
-    // reversedMessage(){
-    //     return this.message.split('').reverse().join('')
-    // },
-    completedTasks() {
-      return this.tasks.filter((task) => task.completed);
-    },
-    incompleteTasks() {
-      return this.tasks.filter((task) => !task.completed);
-    },
-  },
+Vue.component("task", {
+	template: "<li>Foobar</li>",
 });
