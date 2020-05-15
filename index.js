50cd9f0b9c26e6b13ff2f1d003d294a451cb3c61
wright <calebogundiya@gmail.com>
2020-05-15 13:26:38 +0100

    Implemented bulma tabs

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 Vue.config.devtools = true;
 
+Vue.component('wright-tabs', {
+    template: `
+    <div>
+        <div class="tabs">
+            <ul>
+                <li class="is-active"><a>Pictures</a></li>
+                <li><a>Music</a></li>
+                <li><a>Videos</a></li>
+                <li><a>Documents</a></li>
+            </ul>
+        </div>
+
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+    `,
+    mounted() {
+        console.log(this.$children);
+    },
+})
+
+Vue.component('wright-tab', {
+    template: `
+        <div>
+            <slot></slot>
+        </div>
+    `,
+})
+
 Vue.component('wright-modal', {
+    data(){
+        
+    },
     template: `
     <div class="modal is-active">
         <div class="modal-background"></div>
         <div class="modal-content">
-            <!-- Any other Bulma elements you want -->
+            <div class="box">
+            <wright-message title="Modal Head" body="Modal Body"></wright-message>
+            </div>
         </div>
-        <button class="modal-close is-large" aria-label="close"></button>
+        <button class="modal-close is-large" @click="$emit('close')" aria-label="close"></button>
     </div>
     `,
     methods: {
-        toggleModal(){
-            
-        }
     }
 
-})
+});
 
 Vue.component('wright-message', {
     data(){
@@ -42,16 +74,10 @@ Vue.component('wright-message', {
     }
 })
 
-
-
-
-
-
-
 new Vue({
     el: '#app',
     data: {
-        name: 'Caleb'
+        showModal: false
     },
     methods: {
         
